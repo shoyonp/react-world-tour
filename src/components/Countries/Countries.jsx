720f5401233782169845ffcb5b6ee47b16b9a1d0
shoyonp <shoyonkumar143@gmail.com>
2024-10-20 03:30:14 +0600

    added falges and names whiches are visited

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,23 +1,65 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
-    useEffect(()=> {
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data));
-    }, [])
-    return (
-        <div>
-            <h3>Countries {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  const handleVisetedCountry = (country) => {
+    console.log("add this to your visited country");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+    // console.log(newVisitedCountries);
+  };
+
+  const handleVisitedFlags = flag =>{
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  }
+
+//   remove item form an array in a state
+// use filter to select all the elemants expect th eone you want to remove
+
+
+  return (
+    <div>
+      <h3>Countries {countries.length}</h3>
+      {/* visited country */}
+      <div>
+        <h5>Visited Countries {visitedCountries.length}</h5>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="flag-container">
+          {
+            visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+          }
+      </div>
+      {/* display countries */}
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3}
+            handleVisetedCountry={handleVisetedCountry}
+            handleVisitedFlags ={handleVisitedFlags}
+            country={country}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,12 +1,38 @@
-import './Country.css';
-const Country = ({country}) => {
-    const {name, flags} = country;
-    return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
-            <img src={flags.png} alt="" />
-        </div>
-    );
+import { useState } from "react";
+import "./Country.css";
+const Country = ({ country, handleVisetedCountry, handleVisitedFlags }) => {
+  const { name, flags, population, area, cca3 } = country;
+
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+  //   const passWithParams = () => handleVisetedCountry(country);
+
+  return (
+    <div className={`country ${visited ? "visited" : "non-visited"}`}>
+      <h3 style={{ color: visited ? "purple" : "white" }}>
+        Name: {name?.common}
+      </h3>
+      <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>area: {area}</p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
+      <button onClick={() => handleVisetedCountry(country)}>
+        Mark visited
+      </button>
+      <br />
+      <button onClick={() => handleVisitedFlags(country.flags.png)}>
+        Add Flag
+      </button>
+      <br />
+      <button onClick={handleVisited}>{visited ? "Visited" : "going"}</button>
+      <br />
+      {visited ? "I have visited this country" : "I want to visit"}
+    </div>
+  );
 };
 
 export default Country;
